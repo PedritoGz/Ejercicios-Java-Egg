ca0017c3f18d02badcd0074cc3773f14af8c526d
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-14 18:52:20 -0500

    segundo ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -9,15 +9,10 @@ public class JavaEjerciciosEgg {
     public static void main(String[] args) {
        Scanner leer = new Scanner(System.in);
         
-       System.out.println("Ingresa el primer num");
-       int num1 = leer.nextInt();
+       System.out.println("Ingresa tu nombre");
+       String nombre = leer.nextLine();
        
-       System.out.println("Ingresa el segundo num");
-       int num2 = leer.nextInt();
-       
-       int suma = num1 + num2;
-       
-       System.out.println(suma);
+       System.out.println(nombre);
        
         
     }
