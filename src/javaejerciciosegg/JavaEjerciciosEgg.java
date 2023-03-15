1dad6dccf2e3276e7967b49facd3190c71ebd309
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-14 19:13:56 -0500

    cuarto ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -9,11 +9,10 @@ public class JavaEjerciciosEgg {
     public static void main(String[] args) {
        Scanner leer = new Scanner(System.in);
         
-       System.out.println("Ingresa una frase");
-       String frase = leer.nextLine();
-       
-       System.out.println(frase.toLowerCase());
-       System.out.println(frase.toUpperCase());
+       System.out.println("Ingresa los grados c°: ");
+       double c = leer.nextDouble();
+       double f = (32 + c*(9/5));
+       System.out.println("Los grados f°: " + f);
        
         
     }
