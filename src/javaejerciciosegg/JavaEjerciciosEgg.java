9038044e7a1f7bef6d2e33491967d0fd923d15ba
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-14 19:00:47 -0500

    tercer ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -9,10 +9,11 @@ public class JavaEjerciciosEgg {
     public static void main(String[] args) {
        Scanner leer = new Scanner(System.in);
         
-       System.out.println("Ingresa tu nombre");
-       String nombre = leer.nextLine();
+       System.out.println("Ingresa una frase");
+       String frase = leer.nextLine();
        
-       System.out.println(nombre);
+       System.out.println(frase.toLowerCase());
+       System.out.println(frase.toUpperCase());
        
         
     }
