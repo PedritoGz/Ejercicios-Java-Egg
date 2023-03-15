5982ba39161ca5d5f7a95a3e22f7f532bc008db8
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-14 19:21:33 -0500

    quinto ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -9,10 +9,13 @@ public class JavaEjerciciosEgg {
     public static void main(String[] args) {
        Scanner leer = new Scanner(System.in);
         
-       System.out.println("Ingresa los grados c°: ");
-       double c = leer.nextDouble();
-       double f = (32 + c*(9/5));
-       System.out.println("Los grados f°: " + f);
+       System.out.print("Ingresa un num: ");
+       double num = leer.nextDouble();
+       double doble = num*num; 
+       double triple = num*num*num;
+       System.out.println("El doble: " + doble);
+       System.out.println("El triple: " + triple);
+       System.out.println("La raiz: " + Math.sqrt(num));
        
         
     }
