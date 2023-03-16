2d3765eb055bb303ac64a69324798286a3e79863
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-15 21:37:55 -0500

    trece ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -8,26 +8,19 @@ public class JavaEjerciciosEgg {
     
     public static void main(String[] args) {
        Scanner leer = new Scanner(System.in);
-        
-       int contadorCorrectas = 0;
-       int contadorIncorrectas = 0;
-       String frase;
        
-       do{
-          
-          System.out.print("Ingresa una frase: ");
-          frase = leer.nextLine(); 
- 
-          if(frase.length()==5 && "O".equals(frase.substring(4, 5)) && "X".equals(frase.substring(0, 1)) ){
-              contadorCorrectas++;
-          } else {
-              contadorIncorrectas++;
-          }
-       }while(!("&&&&&".equals(frase)));
-       
-        System.out.println("Secuencias correctas: " + contadorCorrectas);
-        System.out.println("Secuencias incorrectas: " + contadorIncorrectas);
+        System.out.println("Ingresa un num: ");
+        int numU = leer.nextInt();
+       for (int i = 0; i < numU; i++) {
+            for (int j = 0; j < numU; j++) {
+                if (i == 0 || i == numU - 1 || j == 0 || j == numU - 1) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println("");
+        }
     }
-    
-    
 }
+
