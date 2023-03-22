93216a05566c276346656e53db3db0a046c01c74
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-22 14:03:41 -0500

    diecisiete ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 
 
  // @author pdrgo
- // Realizar un algoritmo que rellene un vector de tamaño N con valores aleatorios y le pida
- // al usuario un numero a buscar en el vector. El programa mostrará donde se encuentra el
- // numero y si se encuentra repetido
+ // Recorrer un vector de N enteros contabilizando cuántos números son de 1 dígito, cuántos
+ // de 2 dígitos, etcétera (hasta 5 dígitos).
  
 public class JavaEjerciciosEgg {
     
@@ -14,28 +13,44 @@ public class JavaEjerciciosEgg {
         
        Scanner leer = new Scanner(System.in);
        
-        int num[] = new int[10];
-       
-        for(int i = 0; i < 10; i++){
-            num[i]= (int)(Math.random()*10+1);
-        } 
-        System.out.print("Ingresa un num (1-10) para buscar:");
-        int numU = leer.nextInt();
-        int contador = 0;
-        int i;
-        for( i = 0; i < 10; i++){
-            if (numU == num[i]){
-                System.out.println("Esta en la posición: " + (i+1)); 
-                contador++;
+        int num[] = {1,10,55,1012,11001,2,1240,6,111,54789};
+        int contador [] = new int[10];
+        int contadorUno = 0;
+        int contadorDos = 0;
+        int contadorTres = 0;
+        int contadorCuatro = 0;
+        int contadorCinco = 0;
+        for(int i = 0; i < num.length; i++){
+            String numerosCadena = Integer.toString(num[i]);
+            if (numerosCadena.length()<=5){
+                contador[i] = (numerosCadena.length());
             }
+            System.out.println("El numero: " + num[i] + " tiene " + contador[i] + " cifras");
+          
+            
+            if (contador[i]==1){
+                contadorUno++;
+            } 
+            if (contador[i]==2){
+                contadorDos++;
+            } 
+            if (contador[i]==3){
+                contadorTres++;
+            } 
+            if (contador[i]==4){
+                contadorCuatro++;
+            } 
+            if (contador[i]==5){
+                contadorCinco++;
+            } 
         }
-        if (contador == 0 ){   
-                System.out.println("No se encontro similitud");
-            }
-        for(i = 0; i < 10; i++){
-            System.out.print(num[i] + " ");
-        }  
-        System.out.println(" ");
+        System.out.println("Numeros con 1 cifra: " + contadorUno);
+        System.out.println("Numeros con 2 cifras: " + contadorDos);
+        System.out.println("Numeros con 3 cifras: " + contadorTres);
+        System.out.println("Numeros con 4 cifras: " + contadorCuatro);
+        System.out.println("Numeros con 5 cifras: " + contadorCinco);
+        
+        
     }
      
 }
