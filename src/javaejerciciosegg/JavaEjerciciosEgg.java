c03aacb9dc235a3b4a0f9c8c685497ef5a3b391f
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-22 14:32:40 -0500

    dieciocho ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -1,56 +1,42 @@
 package javaejerciciosegg;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 
  // @author pdrgo
- // Recorrer un vector de N enteros contabilizando cuántos números son de 1 dígito, cuántos
- // de 2 dígitos, etcétera (hasta 5 dígitos).
+
+ // Realizar un programa que rellene un matriz de 4 x 4 de valores aleatorios y muestre la
+ // traspuesta de la matriz. La matriz traspuesta de una matriz A se denota por B y se
+ // obtiene cambiando sus filas por columnas (o viceversa).
  
 public class JavaEjerciciosEgg {
     
     public static void main(String[] args) {
         
        Scanner leer = new Scanner(System.in);
+       int num [][] = new int [4][4];
        
-        int num[] = {1,10,55,1012,11001,2,1240,6,111,54789};
-        int contador [] = new int[10];
-        int contadorUno = 0;
-        int contadorDos = 0;
-        int contadorTres = 0;
-        int contadorCuatro = 0;
-        int contadorCinco = 0;
-        for(int i = 0; i < num.length; i++){
-            String numerosCadena = Integer.toString(num[i]);
-            if (numerosCadena.length()<=5){
-                contador[i] = (numerosCadena.length());
-            }
-            System.out.println("El numero: " + num[i] + " tiene " + contador[i] + " cifras");
-          
-            
-            if (contador[i]==1){
-                contadorUno++;
-            } 
-            if (contador[i]==2){
-                contadorDos++;
-            } 
-            if (contador[i]==3){
-                contadorTres++;
-            } 
-            if (contador[i]==4){
-                contadorCuatro++;
-            } 
-            if (contador[i]==5){
-                contadorCinco++;
-            } 
+       for(int i = 0; i < 4; i++){
+           for(int j = 0; j < 4; j++){
+               num[i][j] = (int) (Math.random() * 10);
+           }
+       }
+       System.out.println("Matriz A: ");
+       for(int i = 0; i < 4; i++){
+           for(int j = 0; j < 4; j++){
+               System.out.print(num[i][j] + "    ");
+           }
+           System.out.println("");
+       }
+       System.out.println("Mateiz A transpuesta: ");
+       for(int j = 0; j < 4; j++){
+           for(int i = 0; i < 4; i++){
+               System.out.print(num[i][j] + "    ");
+           }
+           System.out.println("");   
         }
-        System.out.println("Numeros con 1 cifra: " + contadorUno);
-        System.out.println("Numeros con 2 cifras: " + contadorDos);
-        System.out.println("Numeros con 3 cifras: " + contadorTres);
-        System.out.println("Numeros con 4 cifras: " + contadorCuatro);
-        System.out.println("Numeros con 5 cifras: " + contadorCinco);
-        
-        
-    }
      
+    }
+
 }
