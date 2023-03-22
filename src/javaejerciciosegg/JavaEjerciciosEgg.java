0f1fc19c9eaf1e159e820ce8af97a6e81fa59e08
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-22 10:22:21 -0500

    quince ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -3,6 +3,8 @@ package javaejerciciosegg;
 import java.util.Scanner;
 
  // @author pdrgo
+ // Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
+ // muestre por pantalla en orden descendente.
  
 public class JavaEjerciciosEgg {
     
@@ -10,33 +12,14 @@ public class JavaEjerciciosEgg {
         
        Scanner leer = new Scanner(System.in);
        
-        System.out.println("Ingrese la cantidad de Euros");
-        double euros = leer.nextDouble();
-        System.out.println("Convertir a:");
-        System.out.println("1) Dolares");
-        System.out.println("2) Libras");
-        System.out.println("3) Yenes");
-        System.out.print("Elige una Opción: ");
-        int opc = leer.nextInt();
-        cambio(euros, opc);  
+        int numCien[] = new int[101];
+        for(int i = 1; i < 101; i++){
+            numCien[i]=i;
+        }  
+        for(int i = 100; i > 0; i--){
+            System.out.println(numCien[i]);
+        } 
     }
-    
-     public static void cambio(double euros, int opc) {
-         
-       switch(opc){
-           case 1:
-               double dolar = 1.28611 * euros;
-               System.out.println(dolar);
-               break;
-           case 2:
-               double libra = 0.86 * euros;
-               System.out.println(libra);
-               break;
-           case 3:
-               double yenes = 129.852 * euros;
-               System.out.println(yenes);
-               break;
-       } 
-    }  
+     
 }
 
