2464095c014f42b258fa9edb015ee1e8b4b9d37a
Pedro Gonzalez <pdrgonzalez2@hotmail.com>
2023-03-22 11:17:35 -0500

    dieciseis ejem

--- src/javaejerciciosegg/JavaEjerciciosEgg.java
+++ src/javaejerciciosegg/JavaEjerciciosEgg.java
@@ -2,9 +2,11 @@ package javaejerciciosegg;
 
 import java.util.Scanner;
 
+
  // @author pdrgo
- // Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
- // muestre por pantalla en orden descendente.
+ // Realizar un algoritmo que rellene un vector de tamaño N con valores aleatorios y le pida
+ // al usuario un numero a buscar en el vector. El programa mostrará donde se encuentra el
+ // numero y si se encuentra repetido
  
 public class JavaEjerciciosEgg {
     
@@ -12,14 +14,28 @@ public class JavaEjerciciosEgg {
         
        Scanner leer = new Scanner(System.in);
        
-        int numCien[] = new int[101];
-        for(int i = 1; i < 101; i++){
-            numCien[i]=i;
-        }  
-        for(int i = 100; i > 0; i--){
-            System.out.println(numCien[i]);
+        int num[] = new int[10];
+       
+        for(int i = 0; i < 10; i++){
+            num[i]= (int)(Math.random()*10+1);
         } 
+        System.out.print("Ingresa un num (1-10) para buscar:");
+        int numU = leer.nextInt();
+        int contador = 0;
+        int i;
+        for( i = 0; i < 10; i++){
+            if (numU == num[i]){
+                System.out.println("Esta en la posición: " + (i+1)); 
+                contador++;
+            }
+        }
+        if (contador == 0 ){   
+                System.out.println("No se encontro similitud");
+            }
+        for(i = 0; i < 10; i++){
+            System.out.print(num[i] + " ");
+        }  
+        System.out.println(" ");
     }
      
 }
-
